1a5e775311d5dc72a8be0a7c075f035bd61c44a6
Florent <fbryma7@gmail.com>
2023-07-25 11:25:52 +0200

    adding carousel funcionality

--- js/script.js
+++ js/script.js
@@ -23,11 +23,20 @@ function createLineTween(svg) {
 
 //  Carousel JS
 
-const buttons = document.querySelector("[data-carousel-button]");
+const buttons = document.querySelectorAll("[data-carousel-button]");
 
-buttons.ariaReadOnly.array.forEach(element => {
-   buttons.addEventListener("click", () => {
-      
+buttons.forEach(button => {
+   button.addEventListener("click", () => {
+      const offset = button.dataset.carouselButton === "next" ? 1 : -1
+      const slides = button.closest("[data-carousel]").querySelector("[data-slides]")
+
+      const activeSlide = slides.querySelector("[data-active]")
+      let newIndex = [...slides.children].indexOf(activeSlide) + offset
+      if (newIndex < 0 ) newIndex = slides.children.length - 1
+      if(newIndex >= slides.children.length) newIndex = 0
+
+      slides.children[newIndex].dataset.active = true
+      delete activeSlide.dataset.active
    })
 });
 
