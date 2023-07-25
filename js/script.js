a826d4e8bee4cd6430f449bcb2c7f9e140ff9a1c
Florent <fbryma7@gmail.com>
2023-07-25 16:24:17 +0200

    finished desktop version

--- js/script.js
+++ js/script.js
@@ -41,3 +41,54 @@ buttons.forEach(button => {
 });
 
 
+
+
+
+
+
+const lenis = new Lenis({
+   duration: 1.2,
+   easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
+});
+
+function raf(time) {
+   lenis.raf(time);
+   ScrollTrigger.update();
+   requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
+
+const section_1 = document.getElementById("container");
+const col_left = document.querySelector(".col_left");
+const timeln = gsap.timeline({paused: true});
+
+// timeln.fromTo(col_left, {y: 0}, {y: '190vh', duration: 1, ease: 'none'}, 0);
+
+// const scroll_1 = ScrollTrigger.create({
+//    animation: timeln,
+//    trigger: section_1,
+//    start: 'top top',
+//    end: 'bottom center',
+//    scrub: true
+// });
+
+
+
+function handleScroll() {
+   const element = document.querySelector('#menu-container');
+ 
+   const scrollThreshold = 5;
+ 
+   if (window.scrollY > scrollThreshold) {
+     if (!element.classList.contains('scrolled-class')) {
+       element.classList.add('scrolled-class');
+     }
+   } else {
+     element.classList.remove('scrolled-class');
+   }
+ }
+ 
+ window.addEventListener('scroll', handleScroll);
+
+
