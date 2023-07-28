43d3403aaa6ae8183a5c692dd04badaa765900c1
Florent <fbryma7@gmail.com>
2023-07-28 09:56:39 +0200

    adding preloader

--- js/script.js
+++ js/script.js
@@ -116,3 +116,35 @@ function handleScroll() {
    
  }
  
+
+
+//  Preloader Animation 
+
+function loader(_success) {
+   var obj = document.querySelector('.preloader'),
+   inner = document.querySelector('.preloader_inner'),
+   page = document.querySelector('.container');
+   obj.classList.add('show');
+   page.classList.remove('show');
+   var w = 0,
+       t = setInterval(function() {
+           w = w + 1;
+           inner.textContent = w+'%';
+           if (w === 100){
+               obj.classList.remove('show');
+               page.classList.add('show');
+               clearInterval(t);
+               w = 0;
+               if (_success){
+                   return _success();
+               }
+           }
+       }, 20);
+}
+
+
+window.addEventListener("load", () => {
+   setTimeout(function() {
+      loader()
+   }, 1000);
+ });
