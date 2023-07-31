d10110ca019ea0dd0908bc31254e6676a156fbfe
Florent <fbryma7@gmail.com>
2023-07-31 15:07:51 +0200

    test

--- js/script.js
+++ js/script.js
@@ -42,31 +42,8 @@ function startAnimation() {
 // Listen for the scroll event and trigger the animation based on the scroll position
 window.addEventListener('scroll', startAnimation);
 
-// Call the startAnimation function to trigger the initial animation
-startAnimation();
 
 
-
-
-
-// var svg1 = document.querySelector('#svg'+1);
-// var tl = new TimelineMax();
-
-// //create a timeline with 2 tweens that draw 2 separate strokes
-// tl.add(createLineTween(svg1),  "+=2");
-
-// //this function creates a single tween that animates the stroke of an svg
-// function createLineTween(svg) { 
-//    var pathObject = {length:0, pathLength:svg.getTotalLength()}; 
-//    var tween = TweenLite.to(pathObject, 2, {length:pathObject.pathLength, onUpdate:drawLine, onUpdateParams:[pathObject, svg], immediateRender:true});
-//    return tween;
-// };
-
-
-//  //update stroke   
-//  function drawLine(obj, svg) {
-//   svg.style.strokeDasharray = [obj.length, obj.pathLength].join(' ');
-//  };
 
  //  -- Design link: https://dribbble.com/shots/16383090-Personal-Portfolio-Website-concept-design/attachments/9661424?mode=media
 
